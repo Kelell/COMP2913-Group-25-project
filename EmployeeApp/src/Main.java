ceb28334efd1fdf68e4f107ca9b9812637ec1340
Zahoor Ramzan <ll16z4r@leeds.ac.uk>
2019-02-24 23:58:33 +0000

    Login page screen layout

---
+++ EmployeeApp/src/Main.java
@@ -0,0 +1,29 @@
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    private static  Stage mainStage;
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("fxml/loginPage.fxml"));
+        primaryStage.setScene(new Scene(root, 738, 508));
+        primaryStage.setTitle("Bike App");
+        mainStage = primaryStage;
+        mainStage.centerOnScreen();
+        mainStage.show();
+
+    }
+    public void close(){
+        mainStage.close();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
