25ab0cdcae9e3d31344d9d77a0cd828eb4840fd7
Brian Max Tchouambe <sc16bmt@leeds.ac.uk>
2019-02-24 22:19:08 +0000

    Changes

--- BIKEWORLD/src/mypack/BookABike.java
+++ BIKEWORLD/src/mypack/BookABike.java
@@ -38,7 +38,15 @@ public class BookABike extends HttpServlet {
                 "<option value=2>2</option>"+
                 "<option value=3>3</option>"+
                 "<option value=4>4</option>"+
-                "</select>"+
+                "</select>"+ "\n" +
+                "<p>Select a Bike model</p>" + "\n" +
+                "<select name = Model >" +
+                "<option value=0>0</option>" +
+                "<option value=1>1</option>"+
+                "<option value=2>2</option>"+
+                "<option value=3>3</option>"+
+                "<option value=4>4</option>"+
+                "</select>"+ "\n" +
                 "<input type=submit value=Submit>" +
                 "</form>"
         );
