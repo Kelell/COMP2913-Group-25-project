c3cb1f22285b90bd8c743a7ac97cacd64f2e05f0
Zahoor Ramzan <ll16z4r@leeds.ac.uk>
2019-02-25 01:03:01 +0000

    Ticket Data for bike hire duration, price & duration

---
+++ EmployeeApp/src/TicketModel.java
@@ -0,0 +1,56 @@
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
+/**
+ * @author Zahoor
+ */
+public  class TicketModel {
+
+    private StringProperty id;
+    private StringProperty customer_id;
+    private StringProperty bike_id;
+    private StringProperty customerName;
+    private StringProperty startDate;
+    private StringProperty endDate;
+    private StringProperty price;
+    private StringProperty total;
+
+    public TicketModel(String id, String customer_id, String bike_id, String customerName, String startDate, String endDate, String price, String total) {
+        this.id = new SimpleStringProperty(id);
+        this.customer_id = new SimpleStringProperty(customer_id);
+        this.bike_id = new SimpleStringProperty(bike_id);
+        this.customerName = new SimpleStringProperty(customerName);
+        this.startDate = new SimpleStringProperty(startDate);
+        this.endDate = new SimpleStringProperty(endDate);
+        this.price = new SimpleStringProperty("£"+price);
+        this.total = new SimpleStringProperty("£"+total);
+    }
+
+    public StringProperty idProperty() {
+        return id;
+    }
+
+    public StringProperty customer_idProperty() {
+        return customer_id;
+    }
+
+    public StringProperty bike_idProperty() {
+        return bike_id;
+    }
+
+    public StringProperty totalProperty() {
+        return total;
+    }
+    public StringProperty customerNameProperty() {
+        return customerName;
+    }
+    public StringProperty startDateProperty() {
+        return startDate;
+    }
+    public StringProperty endDateProperty() {
+        return endDate;
+    }
+    public StringProperty priceProperty() {
+        return price;
+    }
+}
