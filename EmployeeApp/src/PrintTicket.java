920d0b4e63b3b477085026e766809251606c1361
Zahoor Ramzan <ll16z4r@leeds.ac.uk>
2019-02-25 00:56:27 +0000

    Printing the Ticket as a PDF

---
+++ EmployeeApp/src/PrintTicket.java
@@ -0,0 +1,15 @@
+/**
+ * @author Zahoor
+ */
+public class PrintTicket {
+
+    private TicketModel ticketModel;
+
+    public PrintTicket(TicketModel ticketModel){
+        this.ticketModel = ticketModel;
+    }
+
+    public void printPdf() {
+
+    }
+}
