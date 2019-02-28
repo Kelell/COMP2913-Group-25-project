1cd6557463f09e4a797fab2dd6b13ef027c9e339
Brian Max Tchouambe <sc16bmt@leeds.ac.uk>
2019-02-28 03:37:54 +0000

    Second booking page created

--- BIKEWORLD/src/mypack/BookABike.java
+++ BIKEWORLD/src/mypack/BookABike.java
@@ -8,24 +8,100 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.*;
+import java.util.*;
 
 
 @WebServlet(name = "BookABike")
 public class BookABike extends HttpServlet {
-    protected void doPost(HttpServletRequest request, HttpServletResponse response, String loca) throws ServletException, IOException {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         PrintWriter out = response.getWriter();
         response.setContentType("text/html");
-        out.println("<form action= servlet1 method = 'post' >" +
-                        "<p>Select a Location</p>" + "\n" +
-                        "<select name = Location >" +
-                            "<option value=0>0</option>" +
-                            "<option value=1>1</option>"+
-                            "<option value=2>2</option>"+
-                            "<option value=3>3</option>"+
-                            "<option value=4>4</option>"+
-                        "</select>"+
+
+        jdbc test = new jdbc();
+
+
+        String driver = "com.mysql.cj.jdbc.Driver";
+
+        try {
+            Class.forName(driver);
+            Connection conn = DriverManager.getConnection(test.DB_URL, "EEsET82tG5" ,"UhgQalxiVw");
+
+            Statement stmt = conn.createStatement();
+            String sql;
+            sql = "SELECT BIKE_ID, STATUS, LOCATION, price FROM bike";
+            ResultSet rs = stmt.executeQuery(sql);
+            ArrayList<Integer> bike_ids = new ArrayList<Integer>();
+            ArrayList<Integer> stats = new ArrayList<Integer>();;
+            ArrayList<String> loca = new ArrayList<String>();
+            ArrayList<Float> cost = new ArrayList<Float>();
+
+
+            while(rs.next()){
+                //Retrieve by column name
+                int id  = rs.getInt("BIKE_ID");
+                int status = rs.getInt("STATUS");
+                String location = rs.getString("LOCATION");
+                float price = rs.getFloat("price");
+                bike_ids.add(id);
+                stats.add(status);
+                loca.add(location);
+                cost.add(price);
+            }
+            rs.close();
+            stmt.close();
+            conn.close();
+
+            int listsize = bike_ids.size();
+            String size = Integer.toString(listsize);
+
+            out.println("<head>" +
+                    "<title id = prick >$Title$</title>" +
+                    "<link rel=stylesheet href=style.css type=text/css>" +
+                    "</head>"
+            );
+            out.println("<body>");
+            out.println(
+                    "<div class=nav>"+
+                            "<a  href=index.jsp>Home</a>"+
+                            "<a class=active href=book>Book A Bike</a>" +
+                            "<a href=AboutUs.jsp>About Us</a>"+
+                            "<a href=ContactUs.jsp>Contact Us</a>"+
+                            "<a>Log out</a>" +
+                            "</div>"
+            );
+            int areasize = 0;
+            String t = (String) request.getParameter("Location");
+            for (int i = 0; i < listsize; i++)
+            {
+                String m = (String) loca.get(i);
+                if ( t.equals(m))
+                {
+                    areasize ++;
+                }
+            }
+
+            out.println("<h2> There are currently " + areasize + " bikes in this location</h2>");
+            out.println("<form action= submit method = 'post' >" +
+                    "<p>Select a Bike model</p>" + "\n" +
+                    "<select name = Model >" +
+                    "<option value=0>0</option>" +
+                    "<option value=1>1</option>"+
+                    "<option value=2>2</option>"+
+                    "<option value=3>3</option>"+
+                    "<option value=4>4</option>"+
+                    "</select>"+ "\n" +
+                    "<input type=submit value=Submit>" + "\n" +
                     "</form>"
-        );
+            );
+
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+            out.println("Testing error 1- Failed");
+        } catch (SQLException e) {
+            e.printStackTrace();
+            out.println("Testing error 2- Failed");
+        }
+        out.println("</body>");
 
     }
 
@@ -46,6 +122,11 @@ public class BookABike extends HttpServlet {
             String sql;
             sql = "SELECT BIKE_ID, STATUS, LOCATION, price FROM bike";
             ResultSet rs = stmt.executeQuery(sql);
+            ArrayList<Integer> bike_ids = new ArrayList<Integer>();
+            ArrayList<Integer> stats = new ArrayList<Integer>();;
+            ArrayList<String> loca = new ArrayList<String>();
+            ArrayList<Float> cost = new ArrayList<Float>();
+
 
             while(rs.next()){
                 //Retrieve by column name
@@ -53,20 +134,20 @@ public class BookABike extends HttpServlet {
                 int status = rs.getInt("STATUS");
                 String location = rs.getString("LOCATION");
                 float price = rs.getFloat("price");
-                if (status == 1) {
-                    //Display values
-                    out.println("ID : " + id + "<br>");
-                    out.println("Status : " + status + "<br>");
-                    out.println("Location : " + location + "<br>");
-                    out.println("Price : " + price + "<br>");
-                }
+                bike_ids.add(id);
+                stats.add(status);
+                loca.add(location);
+                cost.add(price);
             }
             rs.close();
             stmt.close();
             conn.close();
 
+            int listsize = bike_ids.size();
+            String size = Integer.toString(listsize);
+
             out.println("<head>" +
-                    "<title>$Title$</title>" +
+                    "<title id = prick >$Title$</title>" +
                     "<link rel=stylesheet href=style.css type=text/css>" +
                     "</head>"
             );
@@ -80,45 +161,18 @@ public class BookABike extends HttpServlet {
                             "<a>Log out</a>" +
                             "</div>"
             );
-
-            out.println("<form action= submit method = 'post' >" +
-                    "<p>Select a Location </p>" + "\n" +
-                    "<select name = Location onchange= myFunction()>" +
+            out.println("<form action= book method = 'post' >" +
+                    "<p>Select a Location</p>" + "\n" +
+                    "<select name = Location >" +
                     "<option value= Please select>Please select</option>" +
-                    "<option value= Alnmouth>Alnmouth</option>" +
-                    "<option value=Beverley>1</option>"+
-                    "<option value=2>2</option>"+
-                    "<option value=3>3</option>"+
+                    "<option value=Alnmouth>Alnmouth</option>" +
+                    "<option value=Beverly>Beverly</option>"+
+                    "<option value=Crystal>Crystal</option>"+
+                    "<option value=Hexam>Hexam</option>"+
                     "<option value=4>4</option>"+
-                    "</select>"+ "\n" +
-                    "<p>Select a Bike model</p>" + "\n" +
-                    "<select name = Model >" +
-                    "<option value=0>0</option>" +
-                    "<option value=1>1</option>"+
-                    "<option value=2>2</option>"+
-                    "<option value=3>3</option>"+
-                    "<option value=4>4</option>"+
-                    "</select>"+ "\n" +
-                    "<p>Select number of bikes</p>" + "\n" +
-                    "<select name = quantity >" +
-                    "<option value=1>1</option>"+
-                    "<option value=2>2</option>"+
-                    "<option value=3>3</option>"+
-                    "<option value=4>4</option>"+
-                    "</select>"+ "\n" +
-                    "<p>Select your duration</p>" + "\n" +
-                    "<select name = Duration >" +
-                    "<option value=1>24 hours</option>" +
-                    "<option value=2>48 hours</option>"+
-                    "<option value=3>72 hours</option>"+
                     "</select>"+ "\n" +
                     "<input type=submit value=Submit>" + "\n" +
-                    "</form>" +
-                    "<script>\n" +
-                    "function myFunction() {" + "\n" +
-                    "doPost(request, response, Alnmouth)" + "\n" +
-                    "}\n" +
-                    "</script>"
+                    "</form>"
             );
 
         } catch (ClassNotFoundException e) {
