9b0f39dd2cc928800f26e0ca09f15809ec8050b3
Zahoor Ramzan <ll16z4r@leeds.ac.uk>
2019-03-09 16:38:54 +0000

    Removed some lines not needed

--- EmployeeApp/src/LoginController.java
+++ EmployeeApp/src/LoginController.java
@@ -1,25 +1,12 @@
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
-import javafx.embed.swing.SwingFXUtils;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
-import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
-import javafx.scene.image.Image;
 import javafx.stage.Stage;
-import sun.misc.BASE64Decoder;
 
-import javax.crypto.Cipher;
-import javax.crypto.spec.SecretKeySpec;
-import javax.imageio.ImageIO;
-import java.awt.image.BufferedImage;
-import java.io.ByteArrayInputStream;
 import java.io.IOException;
-import java.security.Key;
 import java.sql.Connection;
 import java.sql.ResultSet;
 
@@ -35,13 +22,7 @@ public class LoginController {
 
     private static Connection con;
 
-    private static final String ALGORITHM = "AES";
-    private static final byte[] keyValue =
-            new byte[] { 'T', 'h', 'i', 's', 'I', 's', 'A', 'S',
-                    'e', 'c', 'r', 'e', 't', 'K', 'e', 'y' };
-
-    private static final int ITERATIONS = 2;
-
+    
     public void loginButtonHandler() {
         Parent root = null;
 
@@ -77,35 +58,9 @@ public class LoginController {
                     status.setText("Invalid Username or Password");
                 }
             }
-			
+
         }catch (Exception e) {
             e.printStackTrace();
         }
-    }
-
-    public static String decryptedPassword(String value, String salt)
-            throws Exception {
-        Key key = generateKey();
-        Cipher c = Cipher.getInstance(ALGORITHM);
-        c.init(Cipher.DECRYPT_MODE, key);
-
-        String dValue = null;
-        String valueToDecrypt = value;
-        for (int i = 0; i < ITERATIONS; i++) {
-            byte[] decordedValue
-                    = new BASE64Decoder().decodeBuffer(valueToDecrypt);
-            byte[] decValue = c.doFinal(decordedValue);
-            dValue = new String(decValue).substring(salt.length());
-            valueToDecrypt = dValue;
-        }
-        return dValue;
-    }
-
-    private static Key generateKey() throws Exception {
-        Key key = new SecretKeySpec(keyValue, ALGORITHM);
-        // SecretKeyFactory keyFactory
-        //                  = SecretKeyFactory.getInstance(ALGORITHM);
-        // key = keyFactory.generateSecret(new DESKeySpec(keyValue));
-        return key;
     }
 }
