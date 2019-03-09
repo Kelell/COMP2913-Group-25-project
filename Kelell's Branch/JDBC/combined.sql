e916bbb641b5eb2d23457fceea5ac56c344a653d
Zahoor Ramzan <ll16z4r@leeds.ac.uk>
2019-03-09 17:23:42 +0000

    Updated database

--- Kelell's Branch/JDBC/combined.sql
+++ Kelell's Branch/JDBC/combined.sql
@@ -2,7 +2,7 @@ CREATE TABLE IF NOT EXISTS `admin` (
   `username` varchar(50) DEFAULT NULL,
   `password` text,
   `salt` text
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+)
 
 INSERT INTO `admin` (`username`, `password`, `salt`) VALUES
 	('admin', 'admin', NULL);
@@ -13,24 +13,25 @@ CREATE TABLE IF NOT EXISTS `bike` (
   `LOCATION` char(30) DEFAULT NULL,
   `price` double DEFAULT '0',
   PRIMARY KEY (`BIKE_ID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+)
 
 CREATE TABLE IF NOT EXISTS `customer` (
   `CUSTOMER_ID` int(11) NOT NULL AUTO_INCREMENT,
   `CUSTOMER_NAME` char(30) DEFAULT NULL,
   `CUSTOMER_ADDRESS` char(30) DEFAULT NULL,
   PRIMARY KEY (`CUSTOMER_ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8;
+)
 
 INSERT INTO `customer` (`CUSTOMER_ID`, `CUSTOMER_NAME`, `CUSTOMER_ADDRESS`) VALUES
 	(1, 'Eden Hood', '179 Kings Road EXETER EX32 9IB'),
 	(2, 'Vanessa Summers', '459 King Street ENFIELD EN61 4'),
 	(3, 'Zakaria Sanchez', '220 Alexander Road NORTH WEST'),
-	(4, 'Alexander Holden', '7801 School Lane ROCHESTER ME3'),
+	(4, 'Alexander Holden 179 Kings Roa', '7801 School Lane ROCHESTER ME3'),
 	(5, 'Iona Moran', '422 York Road DORCHESTER DT88'),
 	(6, 'Xander Warren', '7801 School Lane ROCHESTER ME3'),
 	(7, 'Sonny Long', '9677 Windsor Road HARROW HA81'),
-	(8, 'Aysha Stein', '8472 High Street HUDDERSFIELD');
+	(8, 'Aysha Stein', '8472 High Street HUDDERSFIELD'),
+	(44, 'Michael Jakes', 'P.O BOX 56334, AVENUE NAIROBI');
 
 INSERT INTO `bike` (`BIKE_ID`, `STATUS`, `LOCATION`, `price`) VALUES
 	(1, 2, 'Alnmouth', 36.79),
@@ -57,20 +58,20 @@ INSERT INTO `bike` (`BIKE_ID`, `STATUS`, `LOCATION`, `price`) VALUES
 	(22, 1, 'Beverly', 28.13);
 	(23, 1, 'Hexam', 35.87)
 
-CREATE TABLE HIRES(
-	HIRE_ID INT PRIMARY KEY NOT NULL,
-	CUSTOMER_ID INT,
-	BIKE_ID INT,
-	START_DATE DATE,
-	END_DATE DATE,
+CREATE TABLE IF NOT EXISTS `hires` (
+  `hire_id` int(11) NOT NULL AUTO_INCREMENT,
+  `CUSTOMER_ID` int(11) DEFAULT NULL,
+  `BIKE_ID` int(11) DEFAULT NULL,
+  `days` int(11) DEFAULT '1',
+  `cash` double DEFAULT '1',
+  `barcode` int(11) DEFAULT '1',
+  `START_DATE` date DEFAULT NULL,
+  `END_DATE` date DEFAULT NULL,
+  PRIMARY KEY (`hire_id`),
+  KEY `CUSTOMER_ID` (`CUSTOMER_ID`),
+  KEY `BIKE_ID` (`BIKE_ID`)
+)
 
-	FOREIGN KEY (CUSTOMER_ID) REFERENCES customer(CUSTOMER_ID),
-	FOREIGN KEY (BIKE_ID) REFERENCES bike(BIKE_ID)
-);
-
-INSERT INTO HIRES VALUES
-  ('1', '1', '8', '2018-12-20', '2018-12-23'),
-  ('2', '3', '9', '2018-10-13', '2018-10-15'),
-  ('3', '5', '12', '2018-05-07', '2018-05-09'),
-  ('4', '8', '15', '2019-01-10', '2019-01-11'),
-  ('5', '7', '19', '2019-02-01', '2019-02-02');
+INSERT INTO `hires` (`hire_id`, `CUSTOMER_ID`, `BIKE_ID`, `days`, `cash`, `barcode`, `START_DATE`, `END_DATE`) VALUES
+	(1, 43, 1, 8, 20, 183237397, '2019-03-06', '2019-03-14'),
+	(2, 44, 2, 3, 10, 49082089, '2019-03-15', '2019-03-18');
