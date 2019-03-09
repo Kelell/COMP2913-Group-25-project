922e8b578f7cfa9a7f7f2b98989883ffa8d6f68a
Zahoor Ramzan <ll16z4r@leeds.ac.uk>
2019-03-09 15:52:32 +0000

    Added comments

--- EmployeeApp/src/Main.java
+++ EmployeeApp/src/Main.java
@@ -4,20 +4,32 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+/**
+ * Start of Application, runs the application instance
+ */
+
 public class Main extends Application {
 
     private static  Stage mainStage;
 
     @Override
     public void start(Stage primaryStage) throws Exception{
+
+
+        //Load Login Page
         Parent root = FXMLLoader.load(getClass().getResource("fxml/loginPage.fxml"));
         primaryStage.setScene(new Scene(root, 738, 508));
+
+       /* Parent root = FXMLLoader.load(getClass().getResource("fxml/dashboard.fxml"));
+        primaryStage.setScene(new Scene(root, 993, 561));*/
         primaryStage.setTitle("Bike App");
         mainStage = primaryStage;
         mainStage.centerOnScreen();
         mainStage.show();
 
     }
+
+    //This method is called to close the window
     public void close(){
         mainStage.close();
     }
