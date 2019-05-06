abfd65b21aedfa6f567ce3206d26b2d9f583b982
Sahil <sahil.ahmed@hotmail.co.uk>
2019-05-06 14:48:48 +0100

    Adapted to further database changes

--- EmployeeApp/src/BookController.java
+++ EmployeeApp/src/BookController.java
@@ -282,7 +282,7 @@ public class BookController {
         String name = names.getText();
 
         //Query to insert data
-        String SQL1 = "INSERT INTO `customer` VALUES(?,?,?,NULL,NULL)";
+        String SQL1 = "INSERT INTO `customer` VALUES(?,?,?,NULL,NULL,NULL)";
 
         try {
 
